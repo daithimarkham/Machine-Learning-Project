5a637317f8ca35ccb9a2ec014e5d026001593477
David Markham <davidmarkham76@gmail.com>
2021-01-10 16:08:29 +0000

    Files updated

--- powerServer.py
+++ powerServer.py
@@ -8,29 +8,29 @@
 # Flask for web app.
 import flask as fl
 from kerasmodel import KerasModel as km
+import numpy as np
+import sys
+import os
 
-
+sys.path.append(os.path.abspath("kerasmodel"))
 
 # Create a new web app.
 # Setting variable to call function.
 app = fl.Flask(__name__, static_url_path="",static_folder="static")  # __name__ is built in variable, the main script in program. 
 
 # Add root route.
-# @app. route("/") is a Python decorator that Flask provides to assign URLs,
-# in our app to functions easily.
+# @app. route("/") is a Python decorator that Flask provides to assign URLs.
+# Home page route
 @app.route("/") # flask command enables function below to have diff mech for input and output.
 def home(): # Index page 
   return app.send_static_file('index.html')
 
 
 # Here we request from our model we previously created.
-@app.route('api/kerasPrediction/<string:wind>')
-def kerasModelPredict(wind): 
-    
-    # String must be converted to float as cannot pass natively
-    # Insight from https://github.com/pallets/flask/issues/315
+@app.route('/api/kerasPrediction/<string:wind>', methods = ['GET', 'POST']) 
+def KerasModelPredict(wind): 
     wind = float(wind)
-    return km.kerasPrediction(wind)
+    return km.kerasPrediction(wind) 
 
 
 
@@ -42,5 +42,6 @@ if __name__ == '__main__':
 # https://towardsdatascience.com/deploying-a-keras-deep-learning-model-as-a-web-application-in-p-fc0f2354a7ff 
 # https://www.tensorflow.org/guide/keras/save_and_serialize 
 # https://github.com/keras-team/keras-io/blob/master/guides/serialization_and_saving.py 
+# https://www.pytorials.com/deploy-keras-model-to-production-using-flask/ 
 # https://getbootstrap.com/
 # https://www.w3schools.com/html/ 
